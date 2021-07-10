5420c216cae8b0e3929a25e047742585d5648b59
Spongiforma <56219994+Spongiforma@users.noreply.github.com>
2021-07-11 01:11:15 +0800

    create login

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import exphbs from 'express-handlebars';
+// import bodyParser from "body-parser";
 dotenv.config();
 
 const port = process.env.SERVER_PORT;
@@ -9,15 +10,17 @@ const app = express();
 
 // app.use(bodyParser.json());
 // app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.engine('hbs',exphbs({defaultLayout: 'main',extname:'hbs'}));
-app.set('views',path.join(__dirname,'views'));
 app.set('view engine','hbs');
 
+app.use(express.static( path.join( __dirname,'..','public' ) ) );
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('login',{
+    title: 'Login',
+  });
 });
 
 app.listen(port);
